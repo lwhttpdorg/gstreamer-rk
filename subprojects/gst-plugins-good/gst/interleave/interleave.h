90224a5ea01879c59f68f80aa3c63a8bc2cf67b4
Niels De Graef <nielsdegraef@gmail.com>
2020-03-16 22:48:58 +0100

    interleave: Use G_DECLARE_FINAL_TYPE

--- subprojects/gst-plugins-good/gst/interleave/interleave.h
+++ subprojects/gst-plugins-good/gst/interleave/interleave.h
@@ -31,16 +31,9 @@
 
 G_BEGIN_DECLS
 
-#define GST_TYPE_INTERLEAVE            (gst_interleave_get_type())
-#define GST_INTERLEAVE(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_INTERLEAVE,GstInterleave))
-#define GST_INTERLEAVE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_INTERLEAVE,GstInterleaveClass))
-#define GST_INTERLEAVE_GET_CLASS(obj) \
-        (G_TYPE_INSTANCE_GET_CLASS ((obj),GST_TYPE_INTERLEAVE,GstInterleaveClass))
-#define GST_IS_INTERLEAVE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_INTERLEAVE))
-#define GST_IS_INTERLEAVE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_INTERLEAVE))
-
-typedef struct _GstInterleave GstInterleave;
-typedef struct _GstInterleaveClass GstInterleaveClass;
+#define GST_TYPE_INTERLEAVE (gst_interleave_get_type())
+G_DECLARE_FINAL_TYPE (GstInterleave, gst_interleave, GST, INTERLEAVE,
+    GstElement)
 
 typedef void (*GstInterleaveFunc) (gpointer out, gpointer in, guint stride, guint nframes);
 
@@ -77,13 +70,6 @@ struct _GstInterleave
 
   gboolean send_stream_start;
 };
-
-struct _GstInterleaveClass
-{
-  GstElementClass parent_class;
-};
-
-GType gst_interleave_get_type (void);
 
 G_END_DECLS
 
