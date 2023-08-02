144ba9df2612595da4a72a64c20c21b8b745b738
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-08-02 16:37:43 -0500

    androidmedia: Support constrained-baseline and constrained-high in AVC profile mapping.
    Additional constant values from Android platform APIs.

--- subprojects/gst-plugins-bad/sys/androidmedia/gstamc.c
+++ subprojects/gst-plugins-bad/sys/androidmedia/gstamc.c
@@ -1213,7 +1213,9 @@ static const struct
       AVCProfileConstrainedHigh, "constrained-high", NULL}, {
       AVCProfileHigh10, "high-10", "high-10-intra"}, {
       AVCProfileHigh422, "high-4:2:2", "high-4:2:2-intra"}, {
-      AVCProfileHigh444, "high-4:4:4", "high-4:4:4-intra"}
+      AVCProfileHigh444, "high-4:4:4", "high-4:4:4-intra"}, {
+      AVCProfileConstrainedBaseline, "constrained-baseline"}, {
+      AVCProfileConstrainedHigh, "constrained-high"}
 };
 
 const gchar *
