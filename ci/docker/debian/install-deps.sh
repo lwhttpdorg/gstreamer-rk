a393f0493ebf5180167972b143876f1d94cfd6a8
Nirbheek Chauhan <nirbheek@centricular.com>
2025-02-08 19:26:55 +0530

    ci: Remove gstreamer from debian and fedora images
    This can interfere with correct testing of plugin loading in the
    install step, and bloats the image massively.
    
    Need newer Meson to avoid a dependency on qt6 lrelease, which pulls in
    gstreamer indirectly.
    
    We now also build our own opencv, to avoid pulling in 1GB of deps.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/8440>

--- ci/docker/debian/install-deps.sh
+++ ci/docker/debian/install-deps.sh
@@ -3,10 +3,10 @@
 set -eux
 
 apt update -y && apt full-upgrade -y
-apt install -y $(<./ci/docker/debian/deps.txt)
+apt install -y --no-install-recommends $(<./ci/docker/debian/deps.txt)
 
 apt remove -y rustc cargo
 
-pip3 install --break-system-packages meson==1.5.2 hotdoc python-gitlab tomli junitparser
+pip3 install --break-system-packages meson==1.6.1 hotdoc python-gitlab tomli junitparser
 
 apt clean all
